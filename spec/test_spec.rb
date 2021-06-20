2c7a41929e205d08f1fa32cfc11602b42b0e082b
masaya-kawamura <lucky.number0128@gmail.com>
2021-06-20 12:27:19 +0000

    Rspecの練習の内容を一旦保存

---
+++ spec/test_spec.rb
@@ -0,0 +1,23 @@
+describe '四則演算' do
+  context '足し算' do
+    it '1 + 1は2になる' do
+      expect(1 + 1).to eq 2
+    end
+  end
+  context '足し算' do
+    it '3 + 2は5になる' do
+      expect(3 + 2).to eq 5
+    end
+  end
+  context '引き算' do
+    it '3-1は2になる' do
+      expect(3-1).to eq 2
+    end
+  end
+  context '引き算' do
+    it'5-2は3になる' do
+      expect(5-2).to eq 3
+    end
+  end
+end
+
