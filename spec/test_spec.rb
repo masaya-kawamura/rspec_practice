2be596690c016c3813c0d69ba83ec3bb2f1a84ab
masaya-kawamura <lucky.number0128@gmail.com>
2021-06-23 13:15:47 +0000

    RSpecの練習。

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,23 +1,75 @@
-describe '四則演算' do
-  context '足し算' do
-    it '1 + 1は2になる' do
-      expect(1 + 1).to eq 2
-    end
+class User
+  def initialize(name:, age:)
+    @name = name
+    @age = age
   end
-  context '足し算' do
-    it '3 + 2は5になる' do
-      expect(3 + 2).to eq 5
-    end
-  end
-  context '引き算' do
-    it '3-1は2になる' do
-      expect(3-1).to eq 2
-    end
-  end
-  context '引き算' do
-    it'5-2は3になる' do
-      expect(5-2).to eq 3
+
+  def greet
+    if @age <= 12
+      "ぼくは#{@name}だよ。"
+    else
+      "僕は#{@name}です。"
     end
   end
 end
 
+
+describe '四則演算' do
+  it '1 + 1 は 2 になること' do
+    expect(1 + 1).to eq 2
+    expect(10 - 1).to eq 9
+    expect(4 * 8).to eq 32
+    expect(40 / 5).to eq 8
+  end
+  it '10 - 1 は 9 になること' do
+    expect(10 - 1).to eq 9
+  end
+end
+
+describe '#greet' do
+  it '12歳以下の場合、ひらがなで答えること' do
+    user = User.new(name: 'たろう', age: 12)
+    expect(user.greet).to eq 'ぼくはたろうだよ。'
+  end
+  it '13歳以上の場合は、感じで答えること' do
+    user = User.new(name: 'たろう', age: 13)
+    expect(user.greet).to eq '僕はたろうです。'
+  end
+end
+
+describe User do
+  describe "#greet" do
+    context '#12歳以下の場合' do
+      it 'ひらがなで答えること' do
+        user = User.new(name: 'たろう', age: 12)
+        expect(user.greet).to eq 'ぼくはたろうだよ。'
+      end
+    end
+    context '13歳以上の場合' do
+      it '漢字で答えること' do
+        user = User.new(name: 'たろう', age: 13)
+        expect(user.greet).to eq '僕はたろうです。'
+      end
+    end
+  end
+end
+
+describe User do
+  describe "#greet" do
+    before do
+      @params = { name: 'たろう'}
+    end
+    context '12際以下の場合' do
+      it 'ひらがなで答えること' do
+        user = User.new(@params.merge(age: 12))
+        expect(user.greet).to eq 'ぼくはたろうだよ。'
+      end
+    end
+    context '13歳以上の場合' do
+      it '感じで答えること' do
+        user = User.new(@params.merge(age: 13))
+        expect(user.greet).to eq '僕はたろうです。'
+      end
+    end
+  end
+end
